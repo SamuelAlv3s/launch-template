ae739f3d3e2dc1d41a82a726254a632e1eb9651c
Samuel <samuelalves681@gmail>
2022-09-11 14:51:02 -0300

    initial template

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss'],
+})
+export class LandingComponent implements OnInit {
+  public form!: FormGroup;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  ngOnInit(): void {
+    console.log(this.form.value);
+  }
+
+  submit() {}
+}
